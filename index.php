823cad399bf1c67d639ca2ae194375cb170ed2db
Fahmi Maulana <fahmi250101@gmail.com>
2024-12-26 06:56:49 +0700

    tambah paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1> Halaman latihan  </h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus est hic amet maiores culpa inventore cupiditate quidem? Quaerat fugiat, quae repellat temporibus excepturi sint. Nihil praesentium magni similique odit rerum?</p>
 </body>
 </html>
