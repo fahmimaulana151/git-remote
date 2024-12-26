24a95974b75b7a64d77788dfb1a5cde3e625fddc
Fahmi Maulana <fahmi250101@gmail.com>
2024-12-26 07:05:18 +0700

    tambah list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1> Halaman latihan  </h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus est hic amet maiores culpa inventore cupiditate quidem? Quaerat fugiat, quae repellat temporibus excepturi sint. Nihil praesentium magni similique odit rerum?</p>
+<ul> 
+    <li> fahmi maulana</li>
+</ul>
+
 </body>
 </html>
